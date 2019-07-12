68995458022eb4d4fdb196eccedaba3b6a805088
David Trussler <mail@davidtrussler.net>
2019-07-12 17:27:16 +0100

    Writes (failing) test

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,8 @@
 describe('My First Test', function() {
-  it('Does not do much!', function() {
-    expect(true).to.equal(false)
+  it('Visits the page and changes the selector to green', function() {
+    cy.visit('http://localhost:5000/index.html');
+    cy.get('select').select('green');
+    cy.get('#testSquare')
+      .should('have.css', 'backgroundColor', 'rgb(0, 128, 0)');
   })
 })
